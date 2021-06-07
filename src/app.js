3e78b8ae615ca90ece9b402bed70a7dd22416899
cmchicas <carlos.chicas@gmail.com>
2021-06-07 10:16:29 -0600

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const app = express();
 
 //setting
 app.set('port', process.env.PORT || 3000)
 
+//cors para permitir peticiones externas
+app.use(cors({origin: "http://localhost:4200"}))
 app.use(morgan('dev'));
 //para entender objetos json
 app.use(express.json());
